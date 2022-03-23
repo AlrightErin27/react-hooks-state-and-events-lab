f459254b925d3cb759eb98b781bffe2cde138e72
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-22 21:16:59 -0700

    done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,17 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
   const [isDark, setIsDark] = useState(false);
-  const appClass = isDark ? "App dark" : "App light";
 
-  function handleClick() {
-    console.log("clicked");
-    setIsDark(!isDark);
-  }
+  const appClass = isDark ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
-        <h2>Shopster</h2>
-        <button onClick={handleClick}>Dark Mode</button>
+        <h2>Shop-ster</h2>
+        <button onClick={() => setIsDark(!isDark)}>
+          {!isDark ? "Change to Dark Mode" : "Change to Light Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterCat, setFilterCat] = useState("All");
+
+  function handleFilter(e) {
+    setFilterCat(e.target.value);
+  }
+  console.log(filterCat);
+
+  //need to use .filter to only display items with matching value,
+  //then map thru filtered items and return rendered items
+  const itemDisplay = items.filter((item) => {
+    if (filterCat === "All") return true;
+    return item.category === filterCat;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
